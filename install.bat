f502f050e7cd4fa5181eae41f65811143c6c79a5
archermancool <57549000+archermancool@users.noreply.github.com>
2019-11-11 16:07:03 -0600

    Add files via upload

--- install.bat
+++ install.bat
@@ -16,12 +16,8 @@ mkdir c:\ArcherOS\UF
 copy /y "INSTALLER\ArcherOS.bat" "c:\ArcherOS"
 copy /y "INSTALLER\apps" c:\ArcherOS\apps"
 copy /y "INSTALLER\ArcherOS.bat" "%userprofile%\desktop"
-cls
-set /p startup=do you want ArcherOS to run on startup? (y or n)
- 
-IF "%startup%"=="y" (
 copy /y "INSTALLER\ArcherOS.bat" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-)
+
 cls
 echo Install worked!
 echo Go to c:\ArcherOS and run ArcherOS.bat to start or open the shortcut on your desktop.
--- update.bat
+++ update.bat
@@ -11,3 +11,8 @@ certutil.exe -urlcache -split -f %1 %2 >nul
 ::-------------------------------------------
 powershell.exe -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('C:\ArcherOS\UF\'); $zip = $shell.NameSpace('C:\ArcherOS\UF\archeros.zip'); $target.CopyHere($zip.Items(), 16); }"
 del c:\ArcherOS\UF\archeros.zip
+copy /y "C:\ArcherOS\UF\archer os\INSTALLER\ArcherOS.bat" "c:\ArcherOS"
+copy /y "C:\ArcherOS\UF\archer os\INSTALLER\apps" c:\ArcherOS\apps"
+copy /y "C:\ArcherOS\UF\archer os\INSTALLER\ArcherOS.bat" "%userprofile%\desktop"
+copy /y "C:\ArcherOS\UF\archer os\INSTALLER\ArcherOS.bat" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+@RD /S /Q "c:\ArcherOS\UF\archer os"
